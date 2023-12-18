510559ccff53c503b69115d63f0a4aebd162f925
ZanhThanh <yoshikaghekira@gmail.com>
2023-12-19 02:18:18 +0700

    capnhat models

---
+++ md2/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DongHoShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
