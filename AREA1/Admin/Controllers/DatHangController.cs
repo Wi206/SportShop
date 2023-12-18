446a4faec3b27408e6183c7129689a70155fd3e5
LongRiver00 <truonggiang07020509@gmail.com>
2023-12-19 02:02:04 +0700

    area

---
+++ AREA1/Admin/Controllers/DatHangController.cs
@@ -0,0 +1,172 @@
+﻿using DongHoShop.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace DongHoShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class DatHangController : Controller
+    {
+        private readonly DongHoShopContext _context;
+
+        public DatHangController (DongHoShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DatHang
+        public async Task<IActionResult> Index ()
+        {
+            var dongHoShopContext = _context.DatHang.Include(d => d.NguoiDung).Include(d => d.TinhTrang);
+            return View(await dongHoShopContext.ToListAsync());
+        }
+
+        // GET: DatHang/Details/5
+        public async Task<IActionResult> Details (int? id)
+        {
+            if (id == null || _context.DatHang == null)
+            {
+                return NotFound();
+            }
+
+            var datHang = await _context.DatHang
+                .Include(d => d.NguoiDung)
+                .Include(d => d.TinhTrang)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (datHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(datHang);
+        }
+
+        // GET: DatHang/Create
+        public IActionResult Create ()
+        {
+            ViewData["NguoiDungID"] = new SelectList(_context.NguoiDung, "ID", "Email");
+            ViewData["TinhTrangID"] = new SelectList(_context.KhachHang, "ID", "MoTa");
+            return View();
+        }
+
+        // POST: DatHang/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create ([Bind("ID,NguoiDungID,TinhTrangID,DienThoaiGiaoHang,DiaChiGiaoHang,NgayDatHang")] DatHang datHang)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(datHang);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["NguoiDungID"] = new SelectList(_context.NguoiDung, "ID", "Email", datHang.NguoiDungID);
+            ViewData["TinhTrangID"] = new SelectList(_context.KhachHang, "ID", "MoTa", datHang.TinhTrangID);
+            return View(datHang);
+        }
+
+        // GET: DatHang/Edit/5
+        public async Task<IActionResult> Edit (int? id)
+        {
+            if (id == null || _context.DatHang == null)
+            {
+                return NotFound();
+            }
+
+            var datHang = await _context.DatHang.FindAsync(id);
+            if (datHang == null)
+            {
+                return NotFound();
+            }
+            ViewData["NguoiDungID"] = new SelectList(_context.NguoiDung, "ID", "Email", datHang.NguoiDungID);
+            ViewData["TinhTrangID"] = new SelectList(_context.KhachHang, "ID", "MoTa", datHang.TinhTrangID);
+            return View(datHang);
+        }
+
+        // POST: DatHang/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit (int id, [Bind("ID,NguoiDungID,TinhTrangID,DienThoaiGiaoHang,DiaChiGiaoHang,NgayDatHang")] DatHang datHang)
+        {
+            if (id != datHang.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(datHang);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DatHangExists(datHang.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["NguoiDungID"] = new SelectList(_context.NguoiDung, "ID", "Email", datHang.NguoiDungID);
+            ViewData["TinhTrangID"] = new SelectList(_context.KhachHang, "ID", "MoTa", datHang.TinhTrangID);
+            return View(datHang);
+        }
+
+        // GET: DatHang/Delete/5
+        public async Task<IActionResult> Delete (int? id)
+        {
+            if (id == null || _context.DatHang == null)
+            {
+                return NotFound();
+            }
+
+            var datHang = await _context.DatHang
+                .Include(d => d.NguoiDung)
+                .Include(d => d.TinhTrang)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (datHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(datHang);
+        }
+
+        // POST: DatHang/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed (int id)
+        {
+            if (_context.DatHang == null)
+            {
+                return Problem("Entity set 'DongHoShopContext.DatHang'  is null.");
+            }
+            var datHang = await _context.DatHang.FindAsync(id);
+            if (datHang != null)
+            {
+                _context.DatHang.Remove(datHang);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DatHangExists (int id)
+        {
+            return (_context.DatHang?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ AREA1/Admin/Controllers/LoaiSanPhamController.cs
@@ -0,0 +1,160 @@
+﻿using DongHoShop.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DongHoShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class LoaiSanPhamController : Controller
+    {
+        private readonly DongHoShopContext _context;
+
+        public LoaiSanPhamController (DongHoShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: LoaiSanPham
+        public async Task<IActionResult> Index ()
+        {
+            return _context.LoaiSanPham != null ?
+                        View(await _context.LoaiSanPham.ToListAsync()) :
+                        Problem("Entity set 'DongHoShopContext.LoaiSanPham'  is null.");
+        }
+
+        // GET: LoaiSanPham/Details/5
+        public async Task<IActionResult> Details (int? id)
+        {
+            if (id == null || _context.LoaiSanPham == null)
+            {
+                return NotFound();
+            }
+
+            var loaiSanPham = await _context.LoaiSanPham
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (loaiSanPham == null)
+            {
+                return NotFound();
+            }
+
+            return View(loaiSanPham);
+        }
+
+        // GET: LoaiSanPham/Create
+        public IActionResult Create ()
+        {
+            return View();
+        }
+
+        // POST: LoaiSanPham/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create ([Bind("ID,TenLoai")] LoaiSanPham loaiSanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(loaiSanPham);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(loaiSanPham);
+        }
+
+        // GET: LoaiSanPham/Edit/5
+        public async Task<IActionResult> Edit (int? id)
+        {
+            if (id == null || _context.LoaiSanPham == null)
+            {
+                return NotFound();
+            }
+
+            var loaiSanPham = await _context.LoaiSanPham.FindAsync(id);
+            if (loaiSanPham == null)
+            {
+                return NotFound();
+            }
+            return View(loaiSanPham);
+        }
+
+        // POST: LoaiSanPham/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit (int id, [Bind("ID,TenLoai")] LoaiSanPham loaiSanPham)
+        {
+            if (id != loaiSanPham.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(loaiSanPham);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LoaiSanPhamExists(loaiSanPham.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(loaiSanPham);
+        }
+
+        // GET: LoaiSanPham/Delete/5
+        public async Task<IActionResult> Delete (int? id)
+        {
+            if (id == null || _context.LoaiSanPham == null)
+            {
+                return NotFound();
+            }
+
+            var loaiSanPham = await _context.LoaiSanPham
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (loaiSanPham == null)
+            {
+                return NotFound();
+            }
+
+            return View(loaiSanPham);
+        }
+
+        // POST: LoaiSanPham/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed (int id)
+        {
+            if (_context.LoaiSanPham == null)
+            {
+                return Problem("Entity set 'DongHoShopContext.LoaiSanPham'  is null.");
+            }
+            var loaiSanPham = await _context.LoaiSanPham.FindAsync(id);
+            if (loaiSanPham != null)
+            {
+                _context.LoaiSanPham.Remove(loaiSanPham);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LoaiSanPhamExists (int id)
+        {
+            return (_context.LoaiSanPham?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
