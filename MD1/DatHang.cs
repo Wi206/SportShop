e151929d11ddc91db6071a70fbc47819c0b44dff
LongRiver00 <truonggiang07020509@gmail.com>
2023-12-19 01:57:18 +0700

    Update

---
+++ MD1/DatHang.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DongHoShop.Models
+{
+    public class DatHang
+    {
+        public int ID { get; set; }
+        [DisplayName("Mã người dùng")]
+        [Required(ErrorMessage = " không được bỏ trống!")]
+        public int NguoiDungID { get; set; }
+
+        [Required(ErrorMessage = " không được bỏ trống!")]
+        public int TinhTrangID { get; set; }
+        [StringLength(20)]
+        [DisplayName("Diện thoại giao hàng")]
+        [Required(ErrorMessage = " không được bỏ trống!")]
+        public string DienThoaiGiaoHang { get; set; }
+        [StringLength(255)]
+        [DisplayName("Địa chỉ giao hàng")]
+        [Required(ErrorMessage = " không được bỏ trống!")]
+        public string DiaChiGiaoHang { get; set; }
+        [DisplayName("Ngày đặt hàng")]
+        [Required(ErrorMessage = " không được bỏ trống!")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = false)]
+        public DateTime NgayDatHang { get; set; }
+        public ICollection<DatHang_ChiTiet>? DatHang_ChiTiet { get; set; }
+        public NguoiDung? NguoiDung { get; set; }
+        public TinhTrang? TinhTrang { get; set; }
+    }
+}
